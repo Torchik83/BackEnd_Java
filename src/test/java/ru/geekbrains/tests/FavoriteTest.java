73428df79d50dfd5502314b9636d5e85d5026b9c
Torchik83 <vvk2383@gmail.com>
2021-08-30 17:23:59 +0300

    Test3

--- src/test/java/ru/geekbrains/tests/FavoriteTest.java
+++ src/test/java/ru/geekbrains/tests/FavoriteTest.java
@@ -9,6 +9,7 @@ import static io.restassured.RestAssured.given;
 public class FavoriteTest extends BaseTest {
 
     String imageDeleteHash;
+    String fileID;
 
     @Test
     void uploadFileTest() {
@@ -22,9 +23,23 @@ public class FavoriteTest extends BaseTest {
                 .body("data.width", CoreMatchers.is(1160))
                 .body("data.size", CoreMatchers.not(0))
                 .when()
-                .post("image/{{imageHash}}/favorite")
+                .post("/image")
                 .prettyPeek()
                 .jsonPath()
                 .get("data.deletehash");
     }
+//    @Test
+//    void postImageFavourite(){
+ //        given()
+//                .headers("Authorization", token)
+//                .log()
+//                .method()
+//                .expect()
+//                .statusCode(200)
+//                .body("success", CoreMatchers.is(true))
+//                .body("data", CoreMatchers.is("favorite"))
+ //               .when()
+//                .post("/image/{imageHash}/favorite", fileID)
+//                .prettyPeek();
+ //   }
 }
